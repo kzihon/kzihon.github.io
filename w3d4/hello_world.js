65a43b8c029b4f9f1ea1888fc867f906a579d08f
kzihon <91498946+kzihon@users.noreply.github.com>
2021-10-14 22:22:44 -0500

    saved

--- w3d4/hello_world.js
+++ w3d4/hello_world.js
@@ -1,2 +1,16 @@
-//setTimeout(function(){console.log("world")},2000);
-console.log("hello")
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+   });
+   readline.question('What is your name? ', name => {
+    console.log(`Welcome ${name}`);
+    readline.question('what is your age? ',age=>{
+        if(age<16){
+            console.log('You are not allowed to drive in Iowa.')
+        }else{
+            console.log("You're allowed to get drivers licende in Iowa.")
+        }
+        readline.close();
+    });
+   
+   });
