a6a4c77d4d088fd52eaa1fd99d559652f7df411f
kzihon <91498946+kzihon@users.noreply.github.com>
2021-10-14 22:22:27 -0500

    saveed

---
+++ w3d4/my_math.js
@@ -0,0 +1,5 @@
+module.exports.add=(x,y)=>x+y;
+module.exports.subtract = (x, y) => x - y;
+module.exports.multiply = (x, y) => x * y;
+module.exports.divide = (x, y) => x / y;
+module.exports.pi=3.14;
